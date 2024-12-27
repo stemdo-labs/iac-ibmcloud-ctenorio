2a3f8a71eec19b6738d5e8877db62cccc80a7fdc
claudiatn <cntenorio@stemdo.io>
2024-12-27 10:18:00 +0100

    Update main.tf

--- iac/Terraform/main.tf
+++ iac/Terraform/main.tf
@@ -138,11 +138,10 @@ resource "ibm_is_instance" "vm_db" {
 }
 
 # IBM Container Registry (ICR)
-resource "ibm_cr_namespace" "rg_namespace" {
-  name              = "ctenorio-cr"
-  resource_group_id = var.resource_group_id
-  region = "eu-gb"
-}
+#resource "ibm_cr_namespace" "rg_namespace" {
+#  name              = "ctenorio-cr"
+#  resource_group_id = var.resource_group_id
+#}
 
 # resource "ibm_resource_instance" "cos_instance" {
 #   name     = "os_instance"
